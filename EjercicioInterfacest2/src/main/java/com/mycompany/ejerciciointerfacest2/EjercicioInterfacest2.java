26166cb205ecb43c20e3bb4d8f0d25d4e39c382f
Snoker <43500707+biblak@users.noreply.github.com>
2025-01-13 17:38:42 +0100

    test connmit
    esto es un test

--- EjercicioInterfacest2/src/main/java/com/mycompany/ejerciciointerfacest2/EjercicioInterfacest2.java
+++ EjercicioInterfacest2/src/main/java/com/mycompany/ejerciciointerfacest2/EjercicioInterfacest2.java
@@ -11,6 +11,6 @@ package com.mycompany.ejerciciointerfacest2;
 public class EjercicioInterfacest2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("test commit!");
     }
 }
